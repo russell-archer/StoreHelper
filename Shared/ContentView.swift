4d70a5b24a830d4f8a100f2251e29a97f3d0b97a
Russell Archer <russell.archer@mac.com>
2021-06-16 15:35:49 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Russell Archer on 16/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/StoreHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreHelperApp.swift
+//  Shared
+//
+//  Created by Russell Archer on 16/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct StoreHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
