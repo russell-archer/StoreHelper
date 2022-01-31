64880cd1e95d6b44b3b5b6cc4d17a538c18e85e5
Russell Archer <russell.archer@mac.com>
2022-01-31 18:24:25 +0000

    Verinfo uses CFBundleDisplayName then CFBundleName
    For the app name we initially look for an entry with the CFBundleDisplayName. This allows the developer to override the Xcode project name (CFBundleName) with a specific name for the app. If this key is missing we default to CFBundleName, which should always be present.
    
    Signed-off-by: Russell Archer <russell.archer@mac.com>

--- Sources/StoreHelper/Views/VersionInfo.swift
+++ Sources/StoreHelper/Views/VersionInfo.swift
@@ -36,8 +36,13 @@ public struct VersionInfo: View {
             .padding()
         }
         .onAppear {
-            // Read the app name, version and release build number from Info.plist
-            if let name = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") { appName = "\(name as? String ?? "StoreHelper")" }
+            // Read the app name, version and release build number from Info.plist.
+            // For the app name we initially look for an entry with the CFBundleDisplayName. This allows the developer to override the
+            // Xcode project name (CFBundleName) with a specific name for the app. If this key is missing we default to CFBundleName,
+            // which should always be present.
+            if let name = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") { appName = "\(name as? String ?? "StoreHelper")" }
+            else if let name = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") { appName = "\(name as? String ?? "StoreHelper")" }
+            
             if let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") { versionInfo = "\(version as? String ?? "Unknown")" }
             if let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") { buildInfo = "\(build as? String ?? "Unknown")" }
         }
