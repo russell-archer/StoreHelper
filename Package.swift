6fc7d5f0b49754b6bf1c249e4145cb332cf2dc88
Russell Archer <russell.archer@mac.com>
2022-04-22 17:31:52 +0100

    Added tvOS to package platform
    Signed-off-by: Russell Archer <russell.archer@mac.com>

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "StoreHelper",
     platforms: [
-        .iOS(.v15), .macOS(.v12)
+        .iOS(.v15), .macOS(.v12), .tvOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
