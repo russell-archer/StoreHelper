c2ffa79e978e47de3929c25e6d744f6443dc84c3
Russell Archer <russell.archer@mac.com>
2024-01-24 13:04:58 +0000

    Updated Package file to specify Swift 5.7
    Signed-off-by: Russell Archer <russell.archer@mac.com>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "swift-collections", url: "https://github.com/apple/swift-collections", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-collections", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
