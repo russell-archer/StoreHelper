9940d175e3eb75586f810491ec7c0ea6b5920f51
Russell Archer <russell.archer@mac.com>
2024-01-24 13:50:16 +0000

    Added Package@swift-5.9.swift to support visionOS
    Signed-off-by: Russell Archer <russell.archer@mac.com>

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "StoreHelper",
+    platforms: [.iOS(.v15), .macOS(.v12), .visionOS(.v1)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "StoreHelper", targets: ["StoreHelper"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/apple/swift-collections", from: "1.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "StoreHelper",
+            dependencies: [.product(name: "Collections", package: "swift-collections")],
+            resources: [.process("Resources")])
+    ]
+)
